65716d5c1b9ade7aa837e4e8f8c8ff4a9243eab5
PranavMarch20 <pranavmac13@gmail.com>
2025-03-02 11:45:10 +0530

    sidebar date dynamic js added

--- scripts.js
+++ scripts.js
@@ -19,3 +19,17 @@ function updateTime() {
 // Update time initially and then every second
 updateTime();
 setInterval(updateTime, 1000);
+
+
+
+// sidebar-date
+function updateDate() {
+    let now = new Date();
+    let options = { month: "short", day: "numeric", year: "numeric" };
+    let formattedDate = now.toLocaleDateString("en-US", options);
+
+    document.getElementById("sidebar-date").textContent = formattedDate;
+}
+
+// Call function to set the date on page load
+updateDate();
